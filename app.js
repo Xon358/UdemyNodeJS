cbae478dfbad2d03b98ab501f201f854f70a5677
Xon358 <acetrou86068@live.fr>
2020-10-16 16:53:26 +0200

    At 050 after debug

--- app.js
+++ app.js
@@ -1,39 +1,6 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
-
-const server = http.createServer((req,res) =>{
-    const url = req.url;
-    const method = req.method;
-    if(url === '/'){
-        res.write("<html>");
-        res.write("<head><title>Enter something</title></head>");
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">POST</button></form></body>');
-        res.write("</html>");
-        return res.end();
-    }
-    if(url === '/message' &&  method === 'POST'){
-        const body = [];
-        req.on('data',(chunk)=>{
-            console.log(chunk);
-            body.push(chunk);
-        });
-        return req.on('end', ()=>{
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split("=")[1];
-            fs.writeFileSync("message.txt",message);
-            res.statusCode = 302;
-            res.setHeader('Location', '/');
-            return res.end();
-        });       
-    }
-    console.log(req.url, req.method, req.headers);
-    res.setHeader('Content-Type', 'text/html');
-    res.write("<html>");
-    res.write("<head><title>test title</title></head>");
-    res.write("<body><p>test p</p></body>");
-    res.write("</html>");
-    res.end();
-});
-
-server.listen(8080);
+console.log(routes.someText);
+const server = http.createServer(routes.handler);
+server.listen(3000);
