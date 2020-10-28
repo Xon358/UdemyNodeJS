edb4e14f44ef3d2b7afc238a12b5114171ca51ea
François J <46707267+Xon358@users.noreply.github.com>
2020-10-28 15:56:17 +0100

    Delete database.js

--- util/database.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('udemy_node', 'root', 'root', {
-  dialect: 'mysql',
-  host: 'localhost',
-  port: '8001'
-});
-
-module.exports = sequelize;
